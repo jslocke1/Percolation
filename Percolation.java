174a1309b06f399218a15ee0000f229bcdb11206
jslocke1 <jslocke888@gmail.com>
2017-04-29 21:44:09 -0400

    Added exception handling

--- Percolation.java
+++ Percolation.java
@@ -1,44 +1,62 @@
 /**
  * Created by jslocke on 4/29/17.
  */
+
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation
 {
-    private int[][] blocks;
+    private boolean[][] isOpen;
+    private WeightedQuickUnionUF grid;
+
     public Percolation(int n)
     {
-        blocks = new int[n][n];
-        for (int i=0;i<blocks.length;i++)
-            for (int j=0;j<blocks.length;j++)
-                blocks[i][j]=0;
+        if (n<0)
+            throw new IllegalArgumentException("n must be greater than zero");
+        isOpen = new boolean[n][n];
+        for (int i = 0; i< isOpen.length; i++)
+            for (int j = 0; j< isOpen.length; j++)
+                isOpen[i][j]=false;
+        grid = new WeightedQuickUnionUF(n);
     }
 
     public void open(int row, int col)
     {
-        blocks[row][col]=1;
+        if (row<=0)
+            throw new IndexOutOfBoundsException("row must be greater than zero");
+        if (col<=0)
+            throw new IndexOutOfBoundsException("col must be greater than zero");
+        isOpen[row-1][col-1]=true;
     }
 
     public boolean isOpen(int row, int col)
     {
-        return blocks[row][col]==1;
+        if (row<=0)
+            throw new IndexOutOfBoundsException("row must be greater than zero");
+        if (col<=0)
+            throw new IndexOutOfBoundsException("col must be greater than zero");
+        return isOpen[row-1][col-1];
     }
 
     public boolean isFull(int row, int col)
     {
-        return blocks[row][col]==2;
+        if (row<=0)
+            throw new IndexOutOfBoundsException("row must be greater than zero");
+        if (col<=0)
+            throw new IndexOutOfBoundsException("col must be greater than zero");
+        return isOpen[row-1][col-1];
     }
 
     public int numberOfOpenSites()
     {
         int numberOfOpenSites=0;
-        for (int i=0;i<blocks.length;i++)
+        for (int i = 0; i< isOpen.length; i++)
         {
-            for (int j = 0; j < blocks.length; j++)
+            for (int j = 0; j < isOpen.length; j++)
             {
-                if (blocks[i][j]==1)
+                if (isOpen[i][j])
                     numberOfOpenSites++;
             }
         }
