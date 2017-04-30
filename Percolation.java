07dc7f5d52e193cc1f70313fa869a1ad7ed5f158
jslocke1 <jslocke888@gmail.com>
2017-04-29 22:37:00 -0400

    wrote method to open coordinates

--- Percolation.java
+++ Percolation.java
@@ -2,14 +2,15 @@
  * Created by jslocke on 4/29/17.
  */
 
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 
 public class Percolation
 {
     private boolean[][] isOpen;
     private WeightedQuickUnionUF grid;
+    final private int TOP = 0;
+    private int BOTTOM;
 
     public Percolation(int n)
     {
@@ -19,7 +20,8 @@ public class Percolation
         for (int i = 0; i< isOpen.length; i++)
             for (int j = 0; j< isOpen.length; j++)
                 isOpen[i][j]=false;
-        grid = new WeightedQuickUnionUF(n);
+        grid = new WeightedQuickUnionUF(n*n+2);
+        final int BOTTOM = n*n+1;
     }
 
     public void open(int row, int col)
@@ -29,6 +31,23 @@ public class Percolation
         if (col<=0)
             throw new IndexOutOfBoundsException("col must be greater than zero");
         isOpen[row-1][col-1]=true;
+        if (row==1)
+            grid.union(row*col,TOP);
+        if (row==isOpen.length)
+            grid.union(row*col, BOTTOM);
+        if (isValidCoordinate(row+1,col) && isOpen(row+1,col))
+            grid.union(row*col,(row+1)*col);
+        if (isValidCoordinate(row-1,col) && isOpen(row-1,col))
+            grid.union(row*col,(row-1)*col);
+        if (isValidCoordinate(row,col+1) && isOpen(row,col+1))
+            grid.union(row*col,row*(col+1));
+        if (isValidCoordinate(row,col-1) && isOpen(row,col-1))
+            grid.union(row*col,row*(col-1));
+    }
+
+    private boolean isValidCoordinate(int row, int col)
+    {
+        return row>=1 && col>=1;
     }
 
     public boolean isOpen(int row, int col)
@@ -46,7 +65,7 @@ public class Percolation
             throw new IndexOutOfBoundsException("row must be greater than zero");
         if (col<=0)
             throw new IndexOutOfBoundsException("col must be greater than zero");
-        return isOpen[row-1][col-1];
+        return grid.connected(TOP,row*col);
     }
 
     public int numberOfOpenSites()
